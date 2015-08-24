2bd3bf5a152f6ffd54bdb5a8aeb9236019c42acf
Mark Greenall <mark@markgreenall.co.uk>
2015-08-24 12:26:41 +0100

    Fixed calculation issue. Passed.

--- GTINCheckDigit.js
+++ GTINCheckDigit.js
@@ -3,19 +3,19 @@ function CheckDigit (gtin) {
     var CheckDigitArray = [];
     var gtinMaths = [3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3];
     var modifier = 17 - (gtin.length - 1); // Gets the position to place first digit in array
-    var gtinCheckDigit = gtin.slice(-1);  // Get provided check digit
-    var BarcodeArray = gtin.split("");    // Split barcode at each digit into array
+    var gtinCheckDigit = gtin.slice(-1);   // Get provided check digit
+    var BarcodeArray = gtin.split("");     // Split barcode at each digit into array
     var gtinLength = gtin.length;
     var tmpCheckDigit = 0;
     var tmpCheckSum = 0;
     var tmpMath = 0;
     
-    // Run through and put chars into array
+    // Run through and put digits into multiplication table
     for (i=0; i < (gtinLength - 1); i++) {
         CheckDigitArray[modifier + i] = BarcodeArray[i];  // Add barcode digits to Multiplication Table
     }
     
-    // Calculate "Sum"
+    // Calculate "Sum" of barcode digits
     for (i=modifier; i < 17; i++) {
         tmpMath = CheckDigitArray[i] * gtinMaths[i];
         tmpCheckSum += tmpMath;
