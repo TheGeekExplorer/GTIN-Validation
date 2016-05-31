94d010a22cf76a93f0091716b4ed40e57a35c7e5
Mark Greenall <mark@markgreenall.uk>
2016-05-31 18:12:52 +0100

    Update GTINCheckDigit.rb

--- RUBY/GTINCheckDigit.rb
+++ RUBY/GTINCheckDigit.rb
@@ -1,25 +1,24 @@
 
 class GTIN
+  
+  def self.check_gtin (gtin)
+    # GTIN math preparations
+    @barcode_array = gtin.split('')                                      # Split GTIN into array
+    @gtin_maths = [3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3]    # Modifers for the GTIN calculation
+    modifier = (17 - gtin.length)                                        # Find the offset for modifer tuple start
+    gtin_check_digit = gtin[-1, 1]                                       # Get the check digit provided
+    gtin_length = gtin.length                                            # GTIN provided's length
     
-	def self.check_gtin (gtin)
-		
-		# GTIN math preparations
-		@barcode_array = gtin.split('')                                      # Split GTIN into array
-		@gtin_maths = [3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3]    # Modifers for the GTIN calculation
-		modifier = (17 - gtin.length)                                        # Find the offset for modifer tuple start
-		gtin_check_digit = gtin[-1, 1]                                       # Get the check digit provided
-		gtin_length = gtin.length                                            # GTIN provided's length
-		
-		# Temporary variables
-		tmpCheckDigit = 0
-		tmpCheckSum = 0
-		tmpMath = 0
-		
-		# TODO
-		
-		return true
-	end
-	
+    # Temporary variables
+    tmpCheckDigit = 0
+    tmpCheckSum = 0
+    tmpMath = 0
+    
+    # TODO
+    
+    return true
+  end
+  
 end
 
 
